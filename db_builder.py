a98fdcd2e803493784432cb861bd73af85ec7bc3
JackieW001 <jwoo@stuy.edu>
2017-10-14 18:52:05 -0400

    finished db_builder

--- db_builder.py
+++ db_builder.py
@@ -1,20 +1,34 @@
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitates CSV I/O
 
+#open csv files
+peeps = csv.DictReader(open("peeps.csv"))
+peeps_value = []
+courses = csv.DictReader(open("courses.csv"))
+
 
 f="discobandit.db"
 
 db = sqlite3.connect(f) #open if f exists, otherwise create
 c = db.cursor()    #facilitate db ops
 
-#==========================================================
-#INSERT YOUR POPULATE CODE IN THIS ZONE
+# create peeps table
+c.execute("CREATE TABLE peeps (name TEXT, age INTEGER, id INTEGER);")
+for i in peeps:
+    c.execute("INSERT INTO peeps VALUES ( '" +
+              i["name"] + "'," +
+              i["age"] + "," +
+              i["id"] +
+              ");")
 
+c.execute("CREATE TABLE courses (code TEXT, mark INTEGER, id INTEGER);")
+for i in courses:
+    c.execute("INSERT INTO courses VALUES ( '" +
+              i["code"] + "'," +
+              i["mark"] + "," +
+              i["id"] +
+              ");")
 
-command = ""          #put SQL statement in this string
-c.execute(command)    #run SQL statement
-
-#==========================================================
 db.commit() #save changes
 db.close()  #close database
 
