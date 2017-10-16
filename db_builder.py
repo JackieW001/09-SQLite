39a2d75861fe75f8749d39cc5ac44fcce2fcd223
JackieW001 <jwoo@stuy.edu>
2017-10-16 19:49:36 -0400

    fixed switch column problem

--- db_builder.py
+++ db_builder.py
@@ -7,17 +7,18 @@ csv_to_sql helper function
 Adds quotes around TEXT data before putting it in INSERT INTO
 List and type_list must be same length
 '''
-def stringify_vals(list, type_list):
+def stringify_vals(val_list, type_list):
     retstr = ""
     ctr = 0
-    for i,j in zip(list,type_list):
+    for i,j in zip(val_list,type_list):
         if j == "TEXT":
             retstr += "'" + i + "'"
         else:
             retstr += i
         ctr += 1
-        if ctr < len(list):
+        if ctr < len(val_list):
             retstr += ","
+    print retstr
     return retstr
 
 '''
@@ -43,7 +44,8 @@ def csv_to_sql(table_name, path_to_csvfile, type_list):
 
     dictReader = csv.DictReader(open(path_to_csvfile))
     keys = dictReader.fieldnames
-
+    print keys
+    
     # collect keys
     if len(keys) != len(type_list):
         raise IndexError("Number of Types does not match number of Fields.")
@@ -53,12 +55,17 @@ def csv_to_sql(table_name, path_to_csvfile, type_list):
     c.execute( "CREATE TABLE " + table_name + " (" + th + ");" )
 
     # inserting values
-    values = []
     for i in dictReader:
-        values = i.values()
+        values = []
+        print i 
+        for key in keys:
+            val = i[key]
+            values.append(val)
+            
         if len(values) != len(type_list):
             raise IndexError("Number of Types does not match number of Values.")
         c_values = stringify_vals(values, type_list)
+        print c_values
         c.execute("INSERT INTO " + table_name + " VALUES (" + c_values + ");" )
          
 
@@ -69,7 +76,7 @@ if __name__ == "__main__":
     db = sqlite3.connect(db_name)  #open if f exists, otherwise create
     c = db.cursor() #facilitate db ops
     
-    csv_to_sql("peeps", "data/peeps.csv", ["INTEGER", "TEXT", "INTEGER"] )
+    csv_to_sql("peeps", "data/peeps.csv", ["TEXT", "INTEGER", "INTEGER"] )
     csv_to_sql("courses", "data/courses.csv", ["TEXT", "INTEGER", "INTEGER"] )
     
     db.commit() # save changes
